13f695b2d89569f5fd3b7f58dbe415540fd8ba33
João Vitor Gomes Silva <34372105+joaokage1@users.noreply.github.com>
2020-12-09 21:25:55 -0300

    Update HelloWorldControllerTest.java
    improve test using matchers

--- Unit-Testing-With-Spring-Boot-And-Mockito/src/test/java/com/example/demo/controller/HelloWorldControllerTest.java
+++ Unit-Testing-With-Spring-Boot-And-Mockito/src/test/java/com/example/demo/controller/HelloWorldControllerTest.java
@@ -1,12 +1,12 @@
 package com.example.demo.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -22,8 +22,10 @@ public class HelloWorldControllerTest {
 		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/hello-world")
 				.accept(org.springframework.http.MediaType.APPLICATION_JSON);
 
-		MvcResult response = this.mockMvc.perform(requestBuilder).andReturn();
-		assertEquals("Hello World", response.getResponse().getContentAsString());
+		this.mockMvc.perform(requestBuilder).andExpect(status().is(200)).andExpect(content().string("Hello World"))
+				.andReturn();
+
+		// assertEquals("Hello World", response.getResponse().getContentAsString());
 
 	}
 
